9e4a3c6cc02921d85519d8eccff932d401bbaa5e
Alfonso Roman <aroman@oid.ucla.edu>
2013-02-25 14:46:38 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+
+    <title>CCLE Responsive Preview</title>
+
+    <!--
+    original: http://www.toddmotto.com/browser-based-responsive-development-tool-viewport-resizing-custom-widths
+     -->
+</head>
+<body>
+    <div id="header">
+
+
+        <ul id="viewports">
+            <li class="reset active">Reset</li>
+            <li data-width="320">320px</li>
+            <li data-width="480">480px</li>
+            <li data-width="540">540px</li>
+            <li data-width="600">600px</li>
+            <li data-width="768">768px</li>
+            <li data-width="900">900px</li>
+            <li data-width="1024">1024px</li>
+            <li data-width="1280">1280px</li>
+            <li data-width="1440">1440px</li>
+        </ul>
+
+<!--                 <form id="url" method="post">
+                    <input id="input" type="text" value="" placeholder="Website URL" autofocus>
+                    <button type="submit">Update</button>
+                </form> -->
+<!--         <form id="addwidth" method="post">
+            <input id="pixel" type="text" value="" placeholder="Custom width, eg. 300">
+            <button type="submit">Add</button>
+        </form> -->
+
+    </div>
+
+    <script type="text/javascript" src="js/responsive.js"></script>
+
+</body>
+</html>
